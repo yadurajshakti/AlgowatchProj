6dc8ad4dde56f33ed855ec5b49b920b97bd3644a
Aman@Aman-PC <yaduraj.shakti@gmail.com>
2017-02-27 00:15:03 +0530

    adde algowatch

---
+++ AlgoWatch/Index.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="AlgoWatch.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Dll Path:
+            <asp:TextBox ID="txtDll" runat="server" Width="512px"></asp:TextBox><br />
+
+            <br />
+            <asp:Button ID="btnGetMethods" runat="server" Text="GetMethods" OnClick="btnGetMethods_Click" /><br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            Input Array:
+             <asp:TextBox ID="txtInput" runat="server" Width="457px" Text="3 5 2 1 6"></asp:TextBox><br />
+
+            Call Method:
+             <asp:TextBox ID="txtMethod" runat="server" Width="457px"></asp:TextBox><br />
+
+            
+            <br />
+            <asp:Button ID="btnCall" runat="server" Text="Call" OnClick="btnCall_Click" /> <br />
+
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AlgoWatch/Index.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AlgoWatch
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetMethods_Click(object sender, EventArgs e)
+        {
+            Assembly myLibrary = Assembly.LoadFile(txtDll.Text);
+
+            // get type of class loaded assembly
+            Type clsType = myLibrary.GetType("AlgorithmsDataStructures.Sorting");
+
+            MethodInfo[] methods = clsType.GetMethods();
+            Label1.Text = "";
+            foreach (var item in methods)
+            {
+                Label1.Text += String.Format("{0}<br />", item.Name);
+            }
+
+        }
+
+        protected void btnCall_Click(object sender, EventArgs e)
+        {
+            string[] a_temp = txtInput.Text.Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+
+            Assembly myLibrary = Assembly.LoadFile(txtDll.Text);
+
+            // get type of class loaded assembly
+            Type clsType = myLibrary.GetType("AlgorithmsDataStructures.Sorting");
+
+            MethodInfo[] methods = clsType.GetMethods();
+
+            if (methods != null)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();//TimeComplexity
+
+                methods.First(s => s.Name == txtMethod.Text.Trim())
+                .Invoke(null, new object[] { a });
+
+                var elapsedMs = watch.ElapsedMilliseconds;
+                Label2.Text = "Time: " + elapsedMs.ToString() + " ms, Total Memory: " + (GC.GetTotalMemory(false) / 1024).ToString() + " MB";
+
+            }
+            else
+            {
+                Label1.Text = "Please methods first";
+            }
+
+
+        }
+
+
+    }
+}
